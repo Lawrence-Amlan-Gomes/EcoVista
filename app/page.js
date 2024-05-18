92fe7bbaf27b8c430dd3b01e7f5d5c44ac7f3b5d
Lawrence_Amlan_Gomes <amlangomes@gmail.com>
2024-05-19 03:46:30 +0600

    Ecovista Done

--- app/page.js
+++ app/page.js
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import LocationDetector from "@/components/LocationDetector";
 
 export default function Home() {
-  return (
-    <div className="h-screen w-full bg-slate-300 flex justify-center items-center">
-      <div className="text-5xl text-blue-900 font-bold">Next JS</div>
-    </div>
-  );
+    return <LocationDetector />;
 }
